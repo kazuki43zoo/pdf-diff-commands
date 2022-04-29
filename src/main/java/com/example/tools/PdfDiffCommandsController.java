1284e14d5c302703819b184963a2ce3235c61d68
Kazuki Shimizu <kazuki43zoo@gmail.com>
2022-04-29 23:02:03 +0900

    Parse option via web request

--- src/main/java/com/example/tools/PdfDiffCommandsController.java
+++ src/main/java/com/example/tools/PdfDiffCommandsController.java
@@ -1,22 +1,48 @@
 package com.example.tools;
 
+import org.apache.pdfbox.rendering.ImageType;
+import org.springframework.beans.BeanUtils;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Paths;
+
 @RestController
 public class PdfDiffCommandsController {
 
-  private final PdfDiffCommandsApplicationRunner runner;
+  private final PdfDiffCommandsProperties properties;
 
-  PdfDiffCommandsController(PdfDiffCommandsApplicationRunner runner) {
-    this.runner = runner;
+  PdfDiffCommandsController(PdfDiffCommandsProperties properties) {
+    this.properties = properties;
   }
 
   @PostMapping("/diff")
   String diff(@RequestBody String command) {
-    runner.run(new DefaultApplicationArguments(command.split(" ")));
+    PdfDiffCommandsProperties requestProperties = new PdfDiffCommandsProperties();
+    BeanUtils.copyProperties(properties, requestProperties);
+    ApplicationArguments arguments = new DefaultApplicationArguments(command.split(" "));
+    if (arguments.containsOption("tools.pdf.image-dpi")) {
+      requestProperties.setImageDpi(Float.parseFloat(arguments.getOptionValues("tools.pdf.image-dpi").get(0)));
+    }
+    if (arguments.containsOption("tools.pdf.image-type")) {
+      requestProperties.setImageType(ImageType.valueOf(arguments.getOptionValues("tools.pdf.image-type").get(0)));
+    }
+    if (arguments.containsOption("tools.pdf.diff-color")) {
+      requestProperties.setDiffColor(arguments.getOptionValues("tools.pdf.diff-color").get(0));
+    }
+    if (arguments.containsOption("tools.pdf.output-dir")) {
+      requestProperties.setOutputDir(Paths.get(arguments.getOptionValues("tools.pdf.output-dir").get(0)));
+    }
+    if (arguments.containsOption("tools.pdf.ignore-ranges")) {
+      arguments.getOptionValues("tools.pdf.ignore-ranges")
+          .forEach(x -> requestProperties.getIgnoreRanges().addAll(StringUtils.commaDelimitedListToSet(x)));
+    }
+    PdfDiffCommandsApplicationRunner runner = new PdfDiffCommandsApplicationRunner(requestProperties);
+    runner.run(arguments);
     return String.valueOf(runner.getExitCode());
   }
 
